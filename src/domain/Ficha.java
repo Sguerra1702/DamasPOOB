2a5cb9819a2d49b0fb623427e69a0997ffb0d6a9
parralol <santinico36@hotmail.com>
2022-11-21 13:12:23 -0500

    matame

--- src/domain/Ficha.java
+++ src/domain/Ficha.java
@@ -4,7 +4,13 @@ public abstract class Ficha {
     protected int pos[];
     protected Color color;
     protected int lives;
-
+    /**
+     *
+     * Constructor de la clase Ficha 
+     * 
+     * @param color color
+     * @param pos   pos 
+     */
     public Ficha(Color color, int[] pos){
 
         this.pos = pos;
@@ -12,13 +18,39 @@ public abstract class Ficha {
         lives += 1; 
         
     }
+
+    /**
+     * Retorna el color de la ficha
+     * 
+     * @return
+     */
     public abstract Color getColor();
 
+    /**
+     * retorna las posiciones
+     * 
+     * @return
+     */
     public abstract int[] getPos();
 
+    /**
+     * define el color de la ficha
+     * 
+     * @param color
+     */
     public abstract void setColor(Color color);
-
+    /**
+     * 
+     * coloca una nueva posicion de la dama
+     * 
+     * @param x
+     * @param y
+     */
     public abstract void setPos(int x, int y);
-
+    /**
+     * 
+     * ejecuta la accion de la ficha
+     * 
+     */
     public abstract void action();
 }
--- src/domain/Jugador.java
+++ src/domain/Jugador.java
@@ -1,11 +1,21 @@
 package domain;
 
 public abstract class Jugador {
-
-    public abstract void makeAMove();
-
+    /**
+     * 
+     * realiza un movimiento
+     * 
+     */
+    public abstract void makeAMove(int fx, int fy, int mx, int my)throws DamasException;
+    /**
+     * retorna el nombre del jugador
+     * @return
+     */
     public abstract String getName();
-
+    /**
+     * retorna el numero de fichas totales en el jugador
+     * @return
+     */
     public abstract int getFichasNum();
     
 }
